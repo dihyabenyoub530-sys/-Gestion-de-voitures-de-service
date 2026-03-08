a99945134ceaf6f27237e7dac439f9fbaacabb08
dihyabenyoub530-sys <dihyabenyoub530@gmail.com>
2026-03-07 23:20:19 -0800

    Ajout la classe voiture

--- atelier4.py
+++ atelier4.py
@@ -13,3 +13,11 @@ class Employe:
                 print("Voiture :", self.voitureService.marque)
             else:
                 print("Pas de voiture de service")
+
+class Voiture:
+    def __init__(self, matricule, annee, marque, kilometrage):
+        self.matricule = matricule
+        self.annee = annee
+        self.marque = marque
+        self.kilometrage = kilometrage
+        self.chauffeur = None
